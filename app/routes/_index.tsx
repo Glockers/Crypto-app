57b5795550b23d8430ca8adb111baf8ad2c1f160
Glockers <megamaxchanel@gmail.com>
2023-05-22 14:56:37 +0300

    init: first commit

---
+++ app/routes/_index.tsx
@@ -0,0 +1,15 @@
+import type { V2_MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "New Remix App" }];
+};
+
+export default function Index() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <Link to={"/demp"}>Go to demo page</Link>
+    </>
+    );
+}
